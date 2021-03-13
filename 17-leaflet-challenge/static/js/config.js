1603fe1592836fc2821c3e0e2d33eedcf9826a3b
AYBEE <ahuertero27@gmail.com>
2021-03-12 19:27:54 -0800

    updating

--- 17-leaflet-challenge/static/js/config.js
+++ 17-leaflet-challenge/static/js/config.js
@@ -1,9 +1,2 @@
 // API key
-<<<<<<< HEAD
-const API_KEY="pk.eyJ1Ijoic3dlZXQ5IiwiYSI6ImNrbHgwZ2xmeTBvaW8yc214Mm9mMG5rNmsifQ.NbYt1Lgbhv2BKYu7Wpo8Vw";
-
-=======
-const API_KEY = "pk.eyJ1Ijoic3dlZXQ5IiwiYSI6ImNrbHgwZ2xmeTBvaW8yc214Mm9mMG5rNmsifQ.NbYt1Lgbhv2BKYu7Wpo8Vw";
-
-//
->>>>>>> 4879af2a0b564608dcc6039c779fd7e8f703bfff
+const API_KEY = "insert key here";
